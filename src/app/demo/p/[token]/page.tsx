7c78a057ee26ec7074acc0914171dca5c6be4332
Hamza Naveed <hamzanaveed.inc@gmail.com>
2026-07-30 09:19:59 -0700

    Implement dynamic redirection in LegacyPersonalizedDemoPage
    Redirects to a dynamic route based on the token parameter.

--- src/app/demo/p/[token]/page.tsx
+++ src/app/demo/p/[token]/page.tsx
@@ -1,2 +1,10 @@
-export { dynamic } from "../../[token]/page";
-export { default } from "../../[token]/page";
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default async function LegacyPersonalizedDemoPage({
+  params,
+}: PageProps<"/demo/p/[token]">) {
+  const { token } = await params;
+  redirect(`/demo/${encodeURIComponent(token)}`);
+}
